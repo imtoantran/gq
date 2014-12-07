e1f07470fa23ef020dad69f9d17bc8fb3c120b8a
imtoantran <imtoantran@gmail.com>
2014-12-07 12:18:31 +0700

    layout index

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include_once('header.php');?>
 <script src="js/jquery.min.js"></script>
+
 <script>
 var banner=1;
 var time_delay=8000;
@@ -92,6 +93,7 @@ function prev_banner(){
 }
 
 </script>
+
 <section class="main" id="banner" style="overflow:hidden">
 	<div class="banner" style="height:300px; overflow:hidden">
         <div style="position:absolute; width:960px; text-align:left; display:block; overflow:hidden; z-index:1" id="divbanner1">
@@ -111,13 +113,12 @@ function prev_banner(){
     		<div style="float:right"><img src="images/icon/next.png" alt="" style="margin:4px 0 -4px 5px" onclick="next_banner()"/></div>
     	</div>
 	</div>
-</section>   
+</section>  
+
 <article class="main" style="margin-bottom:-1px;">
+
 	<section id="shoes">
-    	<div class="left_banner">
-    		<div><img src="images/product/left_banner_1.jpg" alt="" /></div>
-    		<div><img src="images/product/left_banner_2.jpg" alt="" /></div>
-    	</div>
+
     	<div class="big_shoes">
 			<?php
 			$sql = "SELECT * FROM `tintuc` ORDER BY `date` DESC LIMIT 0,2 ";
@@ -130,11 +131,14 @@ function prev_banner(){
                 	<p style="font-weight:bold; height:85px"><?php echo $row['tieude']?></p>                    
                 	<a href="news_detail.php?item=<?php echo $row['id'] ?>"><p style="font-style:italic;border-top:1px solid #000;font-size:12px; padding-top:10px;">Xem chi tiết</p></a>
                 </div>
-                <img src="images/news/<?php echo $row['id'] ?>.jpg"  height=""alt="" />
+                <div class="featured-image-wrap">
+                    <img src="images/news/<?php echo $row['id'] ?>.jpg"  height=""alt="" />
+                </div>
             </div>
 	        <?php }?>
         </div>
         <?php
+            /* featured products */
 			$stt=1;
 			$data_shoes=array();
 			$data_shoes=NULL;
@@ -147,24 +151,64 @@ function prev_banner(){
 				$stt++;
 			}		?>
     	<div class="shoes">
-    		<div><a href="chitiet.php?item=sp-<?php echo $data_shoes[1]['id'] ?>"><img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[1]['id']?>_1.jpg" alt="" /></a></div>
-    		<div><a href="chitiet.php?item=sp-<?php echo $data_shoes[2]['id'] ?>"><img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[2]['id']?>_1.jpg" alt="" /></a></div>
-    		<div><a href="chitiet.php?item=sp-<?php echo $data_shoes[3]['id'] ?>"><img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[3]['id']?>_1.jpg" alt="" /></a></div>
-            <div><a href="chitiet.php?item=sp-<?php echo $data_shoes[4]['id'] ?>"><img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[4]['id']?>_1.jpg" alt="" /></a></div>
-    		<div><a href="chitiet.php?item=sp-<?php echo $data_shoes[5]['id'] ?>"><img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[5]['id']?>_1.jpg" alt="" /></a></div>
-    		<div><a href="chitiet.php?item=sp-<?php echo $data_shoes[6]['id'] ?>"><img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[6]['id']?>_1.jpg" alt="" /></a></div>
-    		<div><a href="chitiet.php?item=sp-<?php echo $data_shoes[7]['id'] ?>"><img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[7]['id']?>_1.jpg" alt="" /></a></div>    		<div><a href="chitiet.php?item=sp-<?php echo $data_shoes[8]['id'] ?>"><img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[8]['id']?>_1.jpg" alt="" /></a></div>            <div class="clr"></div>
+    		<div>
+                <a href="chitiet.php?item=sp-<?php echo $data_shoes[1]['id'] ?>">
+                    <img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[1]['id']?>_1.jpg" alt="" />
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes_blank.jpg" alt="" />
+                </a>
+            </div>
+            <div>
+                <a href="chitiet.php?item=sp-<?php echo $data_shoes[1]['id'] ?>">
+                    <img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[1]['id']?>_1.jpg" alt="" />
+                </a>
+            </div>
+            <div>
+                <a href="chitiet.php?item=sp-<?php echo $data_shoes[1]['id'] ?>">
+                    <img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[1]['id']?>_1.jpg" alt="" />
+                </a>
+            </div>
+            <div>
+                <a href="chitiet.php?item=sp-<?php echo $data_shoes[1]['id'] ?>">
+                    <img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[1]['id']?>_1.jpg" alt="" />
+                </a>
+            </div>
+            <div>
+                <a href="#">
+                    <img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes_blank.jpg" alt="" />
+                </a>
+            </div>
+            <div>
+                <a href="chitiet.php?item=sp-<?php echo $data_shoes[1]['id'] ?>">
+                    <img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[1]['id']?>_1.jpg" alt="" />
+                </a>
+            </div>
+            <div>
+                <a href="chitiet.php?item=sp-<?php echo $data_shoes[1]['id'] ?>">
+                    <img onerror="this.src='images/icon/error.jpg'" src="images/product/shoes/<?php echo $data_shoes[1]['id']?>_1.jpg" alt="" />
+                </a>
+            </div>
     	</div>
         <div class="clr"></div>
     </section>
-    <section id="banner_body">
-    	<div style="background:#0c7bdd;"><img src="images/product/slide/banner_body.jpg" width="749" height="207" alt="" /></div>
-    </section>
-   	<div class="left_banner">
-        <div><img src="images/product/left_banner_3.jpg" /></div>
-        <div><img src="images/product/left_banner_4.jpg" alt="" /></div>
-   	</div>
-   	<section id="">
+
+   	<section id="pk">
+    <div id="navigation">
+        <div>Sản phẩm khác</div>
+        <nav>
+            <ul>
+                <li><a href="#">Giày</a></li>
+                <li><a href="#">Bóp da</a></li>
+                <li><a href="#">Dây nịt</a></li>
+                <li><a href="#">Kính thời trang</a></li>
+                <li><a href="#">Nón</a></li>
+                <li><a href="#">Túi xách</a></li>
+            </ul>
+        </nav>      
+    </div>    
     	<div class="shoes" style="width:840px;">
 			<?php
 			$sql = "SELECT * FROM `pk` WHERE `mucloai`='7' ORDER BY `date` DESC LIMIT 0,3";
@@ -172,11 +216,7 @@ function prev_banner(){
 			while ($row = mysql_fetch_array($result)){?>
             <a href="chitiet.php?item=pk-<?php echo $row['id']?>"><div><img onerror="this.src='images/icon/error.jpg'" src="images/product/other/<?php echo $row['id'] ?>_1.jpg" alt="" /></div></a>
 			<?php }?>
-   			<div class="title_home">
-            	<p>TÚI XÁCH</p>
-                <div class="by_shadow_bl shoes_p2" style="width:150px; margin:1px 0 0 25px; opacity:0.8"> </div>
-                <p class="shoes_p2" style="margin-top:5px;">ĐẲNG CẤP</p>
-            </div>
+
    		</div>
     	<div class="shoes" style="width:840px;">
    			<?php
@@ -185,12 +225,7 @@ function prev_banner(){
 			while ($row = mysql_fetch_array($result)){?>
             <a href="chitiet.php?item=pk-<?php echo $row['id'] ?>"><div><img onerror="this.src='images/icon/error.jpg'" src="images/product/other/<?php echo $row['id'] ?>_1.jpg" alt="" /></div></a>
 			<?php }?>
-   			<div class="title_home">
-            	<div class="by_shadow_bl" style="width:210px;opacity:0.1;"></div>
-                <p>DÂY NỊT</p>
-                <div class="by_shadow_bl shoes_p2" style="width:150px; margin:1px 0 0 25px; opacity:0.8"> </div>
-                <p class="shoes_p2" style="margin-top:5px;">THỜI TRANG</p>
-            </div>
+
    		</div>
     	<div class="shoes" style="width:840px;">
    			<?php
@@ -199,12 +234,6 @@ function prev_banner(){
 			while ($row = mysql_fetch_array($result)){?>
             <a href="chitiet.php?item=pk-<?php echo $row['id'] ?>"><div><img onerror="this.src='images/icon/error.jpg'" src="images/product/other/<?php echo $row['id'] ?>_1.jpg" alt="" /></div></a>
 			<?php }?>
-   			<div class="title_home">
-            	<div class="by_shadow_bl" style="width:210px;opacity:0.2;"></div>
-                <p>BÓP DA PRO</p>
-                <div class="by_shadow_bl shoes_p2" style="width:150px; margin:1px 0 0 25px; opacity:0.8"> </div>
-                <p class="shoes_p2" style="margin-top:5px;">PHONG CÁCH</p>
-            </div>
 
    		</div>
     	<div class="shoes" style="width:840px;">
@@ -214,15 +243,10 @@ function prev_banner(){
 			while ($row = mysql_fetch_array($result)){?>
            	<a href="chitiet.php?item=pk-<?php echo $row['id'] ?>"><div><img onerror="this.src='images/icon/error.jpg'" src="images/product/other/<?php echo $row['id'] ?>_1.jpg" alt="" /></div></a>
 			<?php }?>
-   			<div class="title_home">
-            	<div class="by_shadow_bl" style="width:210px;opacity:0.3;"></div>
-                <p>NÓN</p>
-                <div class="by_shadow_bl shoes_p2" style="width:150px; margin:1px 0 0 25px; opacity:0.8"> </div>
-                <p class="shoes_p2" style="margin-top:5px;">PHONG CÁCH</p>
-            </div>
 
    		</div>
    	</section>
+
     <div class="clr"></div>
 </article>
 <script>slide();</script>
