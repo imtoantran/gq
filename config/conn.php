64195f2e56f2c5a86550ac0fca7278d402889ae9
imtoantran <imtoantran@gmail.com>
2014-12-07 07:12:14 +0700

    clean

--- config/conn.php
+++ config/conn.php
@@ -1,10 +1,19 @@
-<?	$host="localhost";
-	$user="gia467fb";
-	$pass="f2e701b4@!";
-	$dbname = "gia467fb_quoc";
-   
-	$connection= mysql_connect ($host, $user, $pass);
-	if (!$connection) { die ("Can not connect database");}
-	$result = mysql_select_db ($dbname);
-	mysql_query('SET NAMES utf8 COLLATE utf8_general_ci');
+<?	$host="localhost";
+
+	$user="root";
+
+	$pass="";
+
+	$dbname = "giayquocvn";
+
+   
+
+	$connection= mysql_connect ($host, $user, $pass);
+
+	if (!$connection) { die ("Can not connect database");}
+
+	$result = mysql_select_db ($dbname);
+
+	mysql_query('SET NAMES utf8 COLLATE utf8_general_ci');
+
 ?>
